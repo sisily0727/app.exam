5364e4a77eb939dc281abf82542b62f14507bdf1
501-06-PC\501-06 <sisily0727@gmail.com>
2018-01-16 09:59:02 +0900

    20180115

---
+++ 180110 files/src/Exam.java
@@ -0,0 +1,74 @@
+public class Exam {
+    public static void main(String[] args)
+
+
+    {   /* 변수 연습 문제
+        Q1. 문자를 저장하는 변수만들기
+        Q2. 변수에 문자 'a' 저장하기
+        Q3. 변수에 저장한 문자 'a' 출력하기
+         */
+
+        char name;
+        name = 'a';
+
+        
+
+        System.out.println(name);
+
+        /*
+        int num1;
+        num1=10;
+
+        int num2=20;
+        int num3=num1+num2;
+        System.out.println(num1+"+"+num2+"="+num3);
+        */
+
+
+        /*
+        System.out.println("abc"+3);
+        System.out.println(1+4);
+        System.out.println("Welcome to Java");
+        System.out.println("First Java program");
+        System.out.println("abc"+"def");
+
+        System.out.println("Programing! It's fun.");
+
+        System.out.println("My name is Hong Gil Dong");
+        System.out.println("I am 13 years old");
+
+        System.out.println("(@) (@)");
+        System.out.println("(=^.^=)");
+        System.out.println("(-m-m-)");
+
+        System.out.println("I can program well.");
+        System.out.println("Dreams come true.");
+
+        System.out.println("My height");
+        System.out.println(170);
+        System.out.println("My weight");
+        System.out.println("68.600000");
+        System.out.println(5 + "Dan");
+        System.out.println(5+" + "+2+" = "+10);
+
+        System.out.println("item   count   price");
+        System.out.println("pen"     +20+   "100");
+        */
+
+
+
+
+        //작성자 : 김예지
+        //작성일 : 18.01.10
+        //작성이유 : 수업...
+        //.......
+
+        /*
+        작성자 : 김예지
+        작성일 : 18.01.10
+        작성이유 : 수업...
+        .......
+        */
+    }
+
+}
